f47f0cdcad688c7d1f82f41dfc0b1c8b0fd56639
Yaniv Yehezkel <yanivye106@gmail.com>
2025-07-30 13:26:32 +0300

    Add initial project files

---
+++ src/main/java/com/NetWeaver/NetWeaverApplication.java
@@ -0,0 +1,13 @@
+package com.NetWeaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetWeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NetWeaver/NetWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NetWeaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetWeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
